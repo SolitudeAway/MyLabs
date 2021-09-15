621936ba0c0e3d19c0a256d13e613c11c1a6f14c
SolitudeAway <djsashko88@gmail.com>
2021-09-15 12:58:07 +0300

    Добавьте файлы проекта.

---
+++ Lab1/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Lab1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int chilo = 0;
+            chilo = Convert.ToInt32(Console.ReadLine());
+            int t = chilo / 100;
+            chilo = (chilo - t * 100) * 10 + t;
+            Console.WriteLine(chilo);
+        }
+    }
+}
